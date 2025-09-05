a5d548a31765ae10013082cf82f5d96ec1102648
sangam0207 <ssrv2024@gmail.com>
2025-09-05 12:53:07 +0530

    fix: issue cors

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ const port = process.env.APP_PORT || 8000;
 
 // --- CORS setup ---
 const allowedOrigins = [
-  "https://government-chat-bot-iota.vercel.app",   // prod vercel
+  "https://government-chat-bot-iota.vercel.app",
+  "https://ejagruti-admin-panel.vercel.app/",   // prod vercel
   "http://localhost:3000",                         // local dev (Next/React)
   "http://127.0.0.1:3000",
   "http://localhost:5173",                         // local dev (Vite)
@@ -72,15 +73,13 @@ const corsOptions = {
 
 // Use CORS BEFORE routes
 app.use(cors(corsOptions));
-// Handle preflight quickly
-app.options("*", cors(corsOptions));
+
 
 // Body parsers & security
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(helmet());
-// if you serve images/files cross-origin, keep this; otherwise you can remove it
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
 
 // Routes
 app.get("/", (req, res) => {
